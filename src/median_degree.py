431559cbff07d83086b82c77bf7bd0d1f5a6a744
Justin Nand <jnand@users.noreply.github.com>
2016-07-14 00:48:19 -0700

    update control script

--- src/median_degree.py
+++ src/median_degree.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""Calculate the median vertex degree of a Venmo payment graph."""
+"""Calculate the median vertex degree of a real-time streaming Venmo payment graph."""
 
 from __future__ import print_function
 
@@ -9,68 +9,114 @@ import argparse
 import logging as log
 import coloredlogs
 
+from mapper import json_to_edge
+from edge_time_cache import Cache
+from reducer import Reducer
+
 # Debugging
-import pprint as pp
+# import pprint as pp
 # import ipdb; ipdb.set_trace()
 
 
 LOGGING = {
-	'version': 1,
-	'disable_existing_loggers': False,
-	'formatters': {        
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {        
         'simple': { 'format': '%(levelname)s %(message)s'}
     },
 }
 
 
-# --------------------------------------------------------------------
+# -------------
+def emit(value):
+    """Workaround for Python's default rounding behavior"""
+    str_deg = '%.3f' % value
+    integer_deg, sep, decimal_deg = str_deg.partition('.')
+    return '.'.join( [integer_deg, (decimal_deg+'0'*2)[:2]])
+
+
 def main(args):
-	pass
+    lru_edge_cache  = Cache(size=args.window)
+    reduce_node_deg = Reducer()
+
+    if args.control:
+        control = open(args.control, 'r')
+
+    with open(args.input, 'r') as trans, open(args.output, 'w') as outfile:
+        for i, raw in enumerate(trans): 
+
+            # Input control supplied
+            expect = control.readline().rstrip() if args.control else 'NA'
+
+            # Ingest, as raw "events" from file stream
+            log.debug("raw:%i: %s" % (i, raw.rstrip()))     
+            
+            # Map, raw to tuple/edge representation 
+            edge = json_to_edge(raw)
+
+            # Partition, updates by cache bucket
+            diff = lru_edge_cache.update(edge)          
+
+            # Reduce, updates into one value per input event
+            result = reduce_node_deg.update(diff)
+            
+            # Collect, output  
+            emit_result = emit(result)
+            log.info("output (median degree): r= %s  e= %s" % (emit_result,expect))            
+            print(emit(result), file=outfile)           
+    
+            log.debug("\n----------\n")         
+
 
 # --------------------------------------------------------------------
 class ArgparseFormatter(argparse.RawDescriptionHelpFormatter, argparse.ArgumentDefaultsHelpFormatter):
-	pass
+    pass
 
 
-if __name__ == '__main__':	
+if __name__ == '__main__':  
 
-	parser = argparse.ArgumentParser(description=__doc__, 
-				formatter_class=ArgparseFormatter)
+    parser = argparse.ArgumentParser(description=__doc__, 
+                formatter_class=ArgparseFormatter)
 
-	parser.add_argument('-i', '--input', required=True, nargs='?',
-		const='./venmo_input/venmo-trans.txt',
-		default='./venmo_input/venmo-trans.txt',
-		help="file containing json transactions from venmo api")
+    parser.add_argument('-i', '--input', required=True, nargs='?',
+        const='./venmo_input/venmo-trans.txt',
+        default='./venmo_input/venmo-trans.txt',
+        help="file containing json transactions from venmo api")
 
-	parser.add_argument('-o', '--output',
-		default='./venmo_output/output.txt',
-		help="output median vertex degrees, one per transaction")
+    parser.add_argument('-o', '--output',
+        default='./venmo_output/output.txt',
+        help="output median vertex degrees, one per transaction")
 
-	parser.add_argument('-w', '--window', type=int,
-		default=60,
-		help="sliding window (lagging) in seconds")
+    parser.add_argument('-c', '--control', nargs='?',
+        const='./venmo_output/control.txt',
+        help="input control")
 
-	parser.add_argument('-s', '--step', type=int,
-		default=1,
-		help="increment window in steps of size STEP")
+    parser.add_argument('-w', '--window', type=int,
+        default=60,
+        help="sliding window (lagging) in seconds")
 
-	parser.add_argument('-d', '--debug', dest='log_lvl', nargs='?', 
-		const='DEBUG',
-		default='INFO',
-		help="debug log verbosity [CRITICAL, ERROR, WARNING, INFO, DEBUG]")
+    # NOT IMPLEMENTED
+    # parser.add_argument('-s', '--step', type=int,
+    #     default=1,
+    #     help="increment window in steps of size STEP")
 
-	parser.add_argument('-q', '--quiet', dest='log_lvl', action='store_const', 
-		const=100,		
-		help="suppress all messages")
+    parser.add_argument('-d', '--debug', dest='log_lvl', nargs='?', 
+        const='DEBUG',
+        default='INFO',
+        help="debug log verbosity [CRITICAL, ERROR, WARNING, INFO, DEBUG]")
 
-	if len(sys.argv[1:]) == 0:		
-		parser.print_help()
-		parser.exit()
+    parser.add_argument('-q', '--quiet', dest='log_lvl', action='store_const', 
+        const=100,      
+        help="suppress all messages")
 
-	args = parser.parse_args()
+    if len(sys.argv[1:]) == 0:      
+        parser.print_help()
+        parser.exit()
 
-	import logging.config
-	logging.config.dictConfig(LOGGING)
-	coloredlogs.install(level=args.log_lvl)
+    args = parser.parse_args()
 
-	main(args)
+    import logging.config
+    logging.config.dictConfig(LOGGING)
+    coloredlogs.install(level=args.log_lvl)
+
+    main(args)
